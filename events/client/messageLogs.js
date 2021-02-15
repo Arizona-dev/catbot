6bf8b7146f33cd1dc2883c9a3b650f5f9addfa30
Arizona <geekwid@gmail.com>
2021-02-15 02:38:52 +0100

    [fix] updated messageLogs

--- events/client/messageLogs.js
+++ events/client/messageLogs.js
@@ -46,15 +46,37 @@ module.exports = (client, user) => {
     if (!message.author.bot) {
       try {
         const channel_id = channels(message.guild.id);
-
         const channel = client.channels.cache.find(channel => channel.id === channel_id);
+
+        await Discord.Util.delayFor(900);
+
+        // Fetch a couple audit logs than just one as new entries could've been added right after this event was emitted.
+        const fetchedLogs = await message.guild.fetchAuditLogs({
+          limit: 6,
+          type: 'MESSAGE_DELETE'
+        }).catch(() => ({
+          entries: []
+        }));
+
+        const auditEntry = fetchedLogs.entries.find(a =>
+          // Small filter function to make use of the little discord provides to narrow down the correct audit entry.
+          a.target.id === message.author.id &&
+          a.extra.channel.id === message.channel.id &&
+          // Ignore entries that are older than 20 seconds to reduce false positives.
+          Date.now() - a.createdTimestamp < 20000
+        );
+
+        const executor = auditEntry ? auditEntry.executor.tag : 'Inconnu';
 
         if (channel && message.reactions.message.deleted) {
           const embed = new Discord.MessageEmbed()
-            .setTitle(`Un message a été supprimé par ${message.author.tag}`)
+            .setTitle("MESSAGE EFFACÉ")
             .setColor("#1E90FF")
-            .addField("Message", message)
-            .addField("Date de création", `${new Date(message.createdAt).toUTCString()}`)
+            .addField("Auteur", message.author.tag, true)
+            .addField("Supprimé par", executor, true)
+            .addField("Channel", message.channel, true)
+            .addField("Message", message.content || "**Aucun message**")
+            .setFooter(`Message ID: ${message.id} | Author ID: ${message.author.id}`);
 
           channel.send(embed);
         }
